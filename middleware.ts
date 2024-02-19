ef655a066d066bc315da5f4523adde53b2e0284e
abdurrahim220 <z.abdurrahim5@gmail.com>
2024-02-19 16:05:14 +0600

    webhooks for create or update user data

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,14 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 export default authMiddleware({
- 
-  publicRoutes: ['/anyone-can-visit-this-route'],
+
+
+  publicRoutes: ['/api/webhooks/clerk'],
+
   
-  ignoredRoutes: ['/no-auth-in-this-route'],
 });
- 
+
 export const config = {
- 
+
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
