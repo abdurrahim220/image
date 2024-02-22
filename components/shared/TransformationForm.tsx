3fefe5e3f40d721de576e7644ee4176798beafee
abdurrahim220 <z.abdurrahim5@gmail.com>
2024-02-22 21:42:14 +0600

    image crud action done

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -117,7 +117,7 @@ const TransformationForm = ({
     );
     setNewTransformation(null);
     startTransition(async () => {
-      // await updateCredits(userId,creditFee)
+      await updateCredits(userId,-1)
     });
   };
 
--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -1,3 +1,7 @@
+import { dataUrl, debounce, getImageSize } from "@/lib/utils";
+import { CldImage } from "next-cloudinary";
+import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
+
 import Image from "next/image";
 
 const TransformedImage = ({
@@ -29,12 +33,38 @@ const TransformedImage = ({
       </div>
       {image?.publicId && transformationConfig ? (
         <div className="relative">
-
+          <CldImage
+            width={getImageSize(type, image, "width")}
+            height={getImageSize(type, image, "height")}
+            src={image?.publicId}
+            alt="image"
+            sizes={"(max-width: 767px) 100vw, 50vw"}
+            placeholder={dataUrl as PlaceholderValue}
+            className="transformed-image"
+            onLoad={() => {
+              setIsTransforming && setIsTransforming(false);
+            }}
+            onError={() => {
+              debounce(() => {
+                setIsTransforming && setIsTransforming(false);
+              }, 8000)();
+            }}
+            {...transformationConfig}
+          />
+             {isTransforming && (
+            <div className="transforming-loader">
+              <Image 
+                src="/assets/icons/spinner.svg"
+                width={50}
+                height={50}
+                alt="spinner"
+              />
+              <p className="text-white/80">Please wait...</p>
+            </div>
+          )}
         </div>
       ) : (
-        <div className="transformed-placeholder">
-            
-        </div>
+        <div className="transformed-placeholder"></div>
       )}
     </div>
   );
