23eb0ca81ac85de60f76692ca88362ea5c1a8925
abdurrahim220 <z.abdurrahim5@gmail.com>
2024-02-22 22:22:11 +0600

    transformation image done

--- lib/database/models/imageModel.ts
+++ lib/database/models/imageModel.ts
@@ -1,43 +1,44 @@
 import { Schema, model, models, Document, Types } from "mongoose";
 
-
-
 export interface IImage extends Document {
-    title: string;
-    transformationType: string;
-    publicId: string;
-    secureUrl: URL;
-    width?: number;
-    height?: number;
-    config?: object;
-    transformationUrl?: URL;
-    aspectRatio?: URL;
-    color?: string;
-    prompt?: string;
-    author: {
-        _id: string,
-        firstName: string,
-        lastName: string,
-    }
-    createdAt: Date;
-    updatedAt: Date;
+  title: string;
+  transformationType: string;
+  publicId: string;
+  secureURL: String;
+  width?: number;
+  height?: number;
+  config?: object;
+  transformationUrl?: String;
+  aspectRatio?: String;
+  color?: string;
+  prompt?: string;
+  author: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  };
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-const ImageSchema = new Schema({
+const ImageSchema = new Schema(
+  {
     title: { type: String, required: true },
     transformationType: { type: String, required: true },
     publicId: { type: String, required: true },
-    secureUrl: { type: URL, required: true },
+    secureURL: { type: String, required: true },
     width: { type: Number },
     height: { type: Number },
     config: { type: Object },
-    transformationUrl: { type: URL },
-    aspectRatio: { type: URL }, color: { type: String },
+    transformationUrl: { type: String },
+    aspectRatio: { type: String },
+    color: { type: String },
     prompt: { type: String },
-    author: { type: Schema.Types.ObjectId, ref: 'User' },
-}, { timestamps: true })
+    author: { type: Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
 
-
-const Image = models?.Image || model("Image", ImageSchema)
+const Image = models?.Image || model("Image", ImageSchema);
 
 export default Image;
